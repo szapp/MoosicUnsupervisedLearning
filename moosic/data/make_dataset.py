9e8092aa05d317ff5c11c2cb9a6f5642099e6f56
szapp <dev.szapp@gmail.com>
2024-06-19 22:51:32 +0200

    Improve data download integration

--- moosic/data/make_dataset.py
+++ moosic/data/make_dataset.py
@@ -1,19 +1,17 @@
 import os
 import urllib.error
-from pathlib import Path
 from urllib.request import urlretrieve
 
-from dotenv import load_dotenv
+from moosic.config import PROJ_ROOT, RAW_DATA_DIR, logger
 
-if __name__ == "__main__":
-    load_dotenv()
-    src = os.getenv("DATA_URL")
-    root = Path(__file__).parents[2]
-    dst = root / "data" / "raw" / "3_spotify_5000_songs.csv"
-    dst.parent.mkdir(parents=True, exist_ok=True)
-    try:
-        download_path = urlretrieve(src, dst)[0]
-    except urllib.error.HTTPError as e:
-        if str(e).startswith("HTTP Error 404"):
-            raise FileNotFoundError("Dataset unreachable (error 404).") from None
-    print("Dataset downloaded to", Path(download_path).relative_to(root))
+src = os.getenv("DATA_URL")
+dst = RAW_DATA_DIR / "3_spotify_5000_songs.csv"
+dst.parent.mkdir(parents=True, exist_ok=True)
+
+try:
+    download_path = urlretrieve(src, dst)[0]
+except urllib.error.HTTPError as e:
+    if str(e).startswith("HTTP Error 404"):
+        raise FileNotFoundError("Dataset unreachable (error 404).") from None
+
+logger.info(f"Dataset downloaded to {dst.relative_to(PROJ_ROOT)}")
