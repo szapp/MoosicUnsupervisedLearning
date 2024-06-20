d70280ec441840b15ea88e436264f6ddb2952dd0
szapp <dev.szapp@gmail.com>
2024-06-20 07:43:37 +0200

    Update data download

--- moosic/data/make_dataset.py
+++ moosic/data/make_dataset.py
@@ -1,17 +1,46 @@
 import os
 import urllib.error
+from pathlib import Path
 from urllib.request import urlretrieve
 
-from moosic.config import PROJ_ROOT, RAW_DATA_DIR, logger
+import typer
+from loguru import logger
+from tqdm import tqdm
 
-src = os.getenv("DATA_URL")
-dst = RAW_DATA_DIR / "3_spotify_5000_songs.csv"
-dst.parent.mkdir(parents=True, exist_ok=True)
+from moosic.config import RAW_DATA_DIR
 
-try:
-    download_path = urlretrieve(src, dst)[0]
-except urllib.error.HTTPError as e:
-    if str(e).startswith("HTTP Error 404"):
-        raise FileNotFoundError("Dataset unreachable (error 404).") from None
+app = typer.Typer()
 
-logger.info(f"Dataset downloaded to {dst.relative_to(PROJ_ROOT)}")
+
+@app.command()
+def main(output_path: Path = RAW_DATA_DIR / "dataset.csv"):
+
+    src = os.getenv("RAW_DATA_URL")
+    dst = Path(output_path)
+    dst.parent.mkdir(parents=True, exist_ok=True)
+
+    logger.info("Downloading dataset...")
+    try:
+        with DownloadProgress(desc="Downloading", unit="B", unit_scale=True, miniters=1) as p:
+            urlretrieve(src, dst, p.report)
+    except urllib.error.HTTPError as e:
+        if str(e).startswith("HTTP Error 404"):
+            logger.error("Dataset unreachable (error 404).")
+        else:
+            logger.exception("An error occurred while downloading the dataset.")
+    else:
+        logger.success("Downloading dataset complete.")
+
+
+class DownloadProgress(tqdm):
+    """Provides hook to display download progress"""
+
+    def report(self, block_num=1, block_size=1, total_size=None):
+        if total_size is not None:
+            self.total = total_size
+        self.n = min(self.total, block_num * block_size)
+        self.update(0)
+
+
+if __name__ == "__main__":
+    app()
